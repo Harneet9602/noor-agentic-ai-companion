46f0efcea3ef4c5fe3b3de696979aeac464054b2
Harneet Kaur <132899657+Harneet9602@users.noreply.github.com>
2026-02-13 13:18:22 +0530

    Update model version in nodes.py

--- nodes.py
+++ nodes.py
@@ -9,7 +9,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 #Initialize the Model (The Brain)
-llm = ChatGroq(model = 'llama-3.3-70b-versatile',temperature =0)
+llm = ChatGroq(model = 'llama-3.1-8b-instant',temperature =0)
 
 def supervisor_node(state: AgentState):
     system_prompt = (
